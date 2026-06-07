6a66ff4bfb51f49db731202a8e9d03073246be9c
Samuel-Main <patriciosamuel097@gmail.com>
2026-06-07 15:44:23 -0300

    Create K-Fold cross validation

--- supervised-learning/knn/knn_regression.py
+++ supervised-learning/knn/knn_regression.py
@@ -4,7 +4,9 @@ from matplotlib import pyplot as plt
 from pathlib import Path
 
 from sklearn import datasets
+from sklearn.neighbors import KNeighborsRegressor
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold, cross_val_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 
@@ -23,6 +25,11 @@ ROOT_DIR = Path(__file__).parent.parent.parent
 OUTPUT_DIR = ROOT_DIR / 'outputs-results' / 'knn_regression'
 
 print(f"Current working directory: {ROOT_DIR}")
+
+result_mse = []
+result_mae = []
+result_r2 = []
+result_mape = []
 
 def main():
   # -------------------------------------------
@@ -71,35 +78,42 @@ def main():
   X_train = sc.fit_transform(X_train)
   X_test = sc.transform(X_test)
   
+  
   # --------------
   # Model 
   # --------------
+  # knn = KNeighborsRegressor(n_neighbors=3, metric='euclidean')
   knn = KNN(k=3, distance_metric='euclidean', type='regression')
   
-  # --------------
-  # Train
-  # --------------
-  knn.fit(X_train, y_train)
-  y_pred = knn.predict(X_test)
+  # ------------------------------
+  # TRAIN k-fold cross-validation
+  # ------------------------------
+  for i, (train_idx, test_idx) in enumerate(KFold(n_splits=5, shuffle=True, random_state=42).split(X_train)):
+    knn.fit(X_train[train_idx], y_train[train_idx])
+    y_pred = knn.predict(X_train[test_idx])
+    
+    mse = mean_squared_error(y_train[test_idx], y_pred)
+    mae = mean_absolute_error(y_train[test_idx], y_pred)
+    r2 = r2_score(y_train[test_idx], y_pred)
+    mape = mean_absolute_percentage_error(y_train[test_idx], y_pred)
 
-  # --------------
-  # Evaluation
-  # --------------
-  mse = mean_squared_error(y_test, y_pred)
-  mae = mean_absolute_error(y_test, y_pred)
-  r2 = r2_score(y_test, y_pred)
+    result_mse.append(mse)
+    result_mae.append(mae)
+    result_r2.append(r2)
+    result_mape.append(mape)
   
-  mape = mean_absolute_percentage_error(y_test, y_pred)
-  
-  print(f'Mean Squared Error: {mse:.2f}')
-  print(f'Root Mean Squared Error: {np.sqrt(mse):.2f}')
-  print(f'Mean Absolute Error: {mae:.2f}')
-  print(f'Mean Absolute Percentage Error: {mape:.2f}%')
-  print(f'R^2 Score: {r2:.2f}')
+  print(f'Mean Squared Error: {np.mean(result_mse):.2f}')
+  print(f'Root Mean Squared Error: {np.sqrt(np.mean(result_mse)):.2f}')
+  print(f'Mean Absolute Error: {np.mean(result_mae):.2f}')
+  print(f'Mean Absolute Percentage Error: {np.mean(result_mape):.2f}%')
+  print(f'R^2 Score: {np.mean(result_r2):.2f}')
   
   # ----------------------------
   # Plot results
   # ----------------------------
+  
+  y_pred = knn.predict(X_test)
+  
   plot_regression_results(
     X_train=sc.inverse_transform(X_train), 
     y_train=y_train, 
